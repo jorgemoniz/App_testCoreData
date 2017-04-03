c1c9fdd965004ac6a03f357213a2658b4839788a
Jorge Moñiz <jorgemoniz@live.com>
2017-04-03 20:29:18 +0200

    Initial Commit

---
+++ App_IntroCoreData/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  App_IntroCoreData
+//
+//  Created by Jorge Moñiz on 3/4/17.
+//  Copyright © 2017 jorgemoniz. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
